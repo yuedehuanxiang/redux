5ae38345b29b6aa37ae956b03bad879483a0afb6
chenshuai <384619043@qq.com>
2019-03-06 22:45:49 +0800

    todolist delete

--- src/store/reducer.js
+++ src/store/reducer.js
@@ -1,6 +1,6 @@
 const defaultState = {
-  inputValue: "123",
-  list: ["1", "2"]
+  inputValue: "",
+  list: ["vue", "react"]
 };
 // reducer 可以接受state, 但是绝不能修改state
 export default (state = defaultState, action) => {
@@ -14,6 +14,11 @@ export default (state = defaultState, action) => {
     const newState = JSON.parse(JSON.stringify(state));
     newState.list.push(newState.inputValue);
     newState.inputValue = "";
+    return newState;
+  }
+  if (action.type === "delete_todo_item") {
+    const newState = JSON.parse(JSON.stringify(state));
+    newState.list.splice(action.index, 1);
     return newState;
   }
   return state;
